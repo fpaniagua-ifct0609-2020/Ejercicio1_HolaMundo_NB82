b8be5fc9dd1f5f7708f6552f392c09a117242c22
fpaniagua <fpaniagua.ifct0609.2020@gmail.com>
2020-02-27 13:49:27 +0100

    Initial commit

---
+++ src/com/fernandopaniagua/ejercicio1/Ejecutador.java
@@ -0,0 +1,19 @@
+package com.fernandopaniagua.ejercicio1;
+
+/**
+ * Ejecutador del proyecto
+ * 
+ * @author FPA
+ */
+public class Ejecutador {
+    /**
+     * Método principal
+     * 
+     * @param args Parámetros
+     */
+    public static void main(String[] args) {
+        //Creamos una instancia de la clase saludador
+        Saludador saludador = new Saludador();
+        saludador.saludar();
+    }
+}
---
+++ src/com/fernandopaniagua/ejercicio1/Saludador.java
@@ -0,0 +1,20 @@
+package com.fernandopaniagua.ejercicio1;
+
+/**
+ * Esta clase saluda.
+ * 
+ * @author FPA
+ * @version 1.0
+ */
+public class Saludador {
+    /**
+     * Escribe 'Hola mundo' en la salida estándar.
+     */
+    public void saludar() {
+        /*
+        El método saludar muestra por la consola
+        un texto.
+         */
+        System.out.println("Hola mundo");
+    }
+}
